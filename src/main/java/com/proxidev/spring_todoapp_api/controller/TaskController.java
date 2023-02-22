a8367d8ff0cfa226fda43ab9ba985feedca3f60d
proxidev <joeledmond95@gmail.com>
2023-02-22 18:23:47 +0100

    initial commit for spring boot tod list

--- src/main/java/com/proxidev/spring_todoapp_api/controller/TaskController.java
+++ src/main/java/com/proxidev/spring_todoapp_api/controller/TaskController.java
@@ -2,11 +2,13 @@ package com.proxidev.spring_todoapp_api.controller;
 
 import com.proxidev.spring_todoapp_api.models.Task;
 import com.proxidev.spring_todoapp_api.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+    @Autowired
     private TaskRepository taskRepository;
     @GetMapping("/")
     public Iterable<Task> getTasks(){
